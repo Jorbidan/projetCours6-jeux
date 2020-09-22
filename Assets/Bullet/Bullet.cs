682779cff0251a5d4781d209cc60138d9371caa3
JORDAN\Utilisateur <jordan.cote@outlook.com>
2020-09-22 15:14:28 -0400

    nouveau commit pusher!

---
+++ Assets/Bullet/Bullet.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    Rigidbody myRigidBody;
+    public int lifeSpan = 10;
+    public float bulletSpeed = 1000;
+    public int bulletDamage = 1;
+    private float timeLeftToLive;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody>();
+        timeLeftToLive = lifeSpan;
+        Destroy(gameObject, lifeSpan);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 position = transform.position;
+        position += transform.forward * Time.deltaTime * bulletSpeed;
+        myRigidBody.MovePosition(position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Damageable damageableObject = other.GetComponent<Damageable>();
+        if (damageableObject != null)
+        {
+            damageableObject.TakeDamage(1);
+        }
+    }
+}
---
+++ Assets/Controller/Spawner/Spawner.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float spawnRate = 1;
+        private float timeLeftBeforeSpawn = 0;
+    private SpawnPoint[] spawnPoints;
+    public GameObject ennemiCubePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPoints = FindObjectsOfType<SpawnPoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateSpawn();
+    }
+    private void UpdateSpawn()
+    {
+        timeLeftBeforeSpawn -= Time.deltaTime;
+        if (timeLeftBeforeSpawn <= 0)
+        {
+            SpawnEnnemieCube();
+            timeLeftBeforeSpawn = spawnRate;
+        }
+    }
+
+    private void SpawnEnnemieCube()
+    {
+        int countSpawnPoints = spawnPoints.Length;
+        int randomSpawnPoint = Random.Range(0, countSpawnPoints);
+        SpawnPoint currentSpawnPoint = spawnPoints[randomSpawnPoint];
+        Instantiate(ennemiCubePrefab, currentSpawnPoint.transform.position, Quaternion.identity);
+    }
+}
